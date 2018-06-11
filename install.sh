c20c8a94fb53b57d11e127ade6b6b1d819f61ce1
Ben Kim <ben.kim@aeris.net>
2018-06-11 15:34:16 -0700

    initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install -y usb-modeswitch
+sudo apt-get install -y gammu
+mkdir -p $HOME/.nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install node
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo rm -rf "$NVM_DIR"
+sudo apt-get purge --auto-remove -y gammu
+sudo apt-get purge --auto-remove -y usb-modeswitch
